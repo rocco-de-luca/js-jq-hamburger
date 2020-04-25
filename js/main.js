5b3205232d7400713e4466d6aef9f28cdca6ddee
rocco-de-luca <roccodelucaboolean@gmail.com>
2020-04-25 14:02:17 +0200

    js-jq-hamburger

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-$(document).ready(function()
-{
-    $('.header-right > a').click(function()
-    {
-        $(".hamburger-menu").show();
-        $(".hamburger-menu").addclass('active');
-    });
-    $(".close").click(function()
-    {
-        $(".hamburger-menu").hide();
-        $(".hamburger-menu").addclass('active');
-    });
+//jquery lezione 1
+
+//referenze
+var hamburgerIcon = $('.header-right > a');
+var closeIcon = $('.hamburger-menu .close');
+var menu = $('.hamburger-menu');
+
+//mostrare il menu
+$(document).ready(function(){
+hamburgerIcon.click(function(){
+menu.addClass('.active');// addClass() aggiunge una classe
 });
+//nascondere menu
+closeIcon.click(function(){
+    menu.removeClass(".active") // removeClass() rimuovere una classe
+});
+});
+     
+
+    
+
+
+
+
